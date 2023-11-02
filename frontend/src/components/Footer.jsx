f4be249df92dbdb3640901b01f2c2daf3b6bd66f
joellekounde <djokounde@gmail.com>
2023-11-02 15:37:29 +0100

    Newsletter

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,29 +1,33 @@
-import React from "react";
-import "../Style/footer.css";
+import "../styles/footer.css";
+import "../styles/App.css";
+import Newsletter from "./Newsletter";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="logo">
         <h1>
-          She<span>Tech</span>Job
+          she<span>tech</span>jobs
         </h1>
       </div>
       <div className="paragraphs">
-        <div className="nosOffres">
-          <h2>Nos offres</h2>
-          <p>Emplois en CDI</p>
-          <p>Emplois en CDD</p>
-          <p>Contrats Pro/ Alternances</p>
-          <p>Stages</p>
+        <div className="footer_links">
+          <div className="nosOffres">
+            <h2>Nos offres</h2>
+            <p>Emplois en CDI</p>
+            <p>Emplois en CDD</p>
+            <p>Contrats Pro/ Alternances</p>
+            <p>Stages</p>
+          </div>
+          <div className="aPropos">
+            <h2>A propos</h2>
+            <p>Qui sommes-nous </p>
+            <p>Conditions générales</p>
+            <p>Vie privée/ Cookies </p>
+            <p>Nous contactez</p>
+          </div>
         </div>
-        <div className="aPropos">
-          <h2>A propos</h2>
-          <p>Qui sommes-nous </p>
-          <p>Conditions générales</p>
-          <p>Vie privée/ Cookies </p>
-          <p>Nous contactez</p>
-        </div>
+        <Newsletter />
       </div>
     </div>
   );
