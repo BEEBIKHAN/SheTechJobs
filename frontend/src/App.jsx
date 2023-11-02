bd9d535b89ab6a0ee5fe1e0d6e2a10a0de06d152
Mamou Skn <mamou.test@gmail.com>
2023-11-02 14:56:09 +0100

    création des routes back et front ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import Home from "./pages/Home";
-
 import "./App.css";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Navbar />
+      <SearchBar />
+      <Content />
+      <Footer />
+      <p>We got this y'all !!!</p>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <h2>This is the Footer</h2>
+      <h3>SheTechJobs</h3>
+      <p>Codé avec le ❤️ par Maurane, Beebi, LeM et Djodjo !</p>
+    </footer>
+  );
+}
