62692597bc989b967b9e32bc2667c102f410d2e0
Mamou Skn <mamou.test@gmail.com>
2023-11-06 16:59:18 +0100

    creation_navbar_top ok

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,42 +1,64 @@
-// import { Link } from "react-router-dom";
-// import SearchBar from "./SearchBar";
-// import LOGO from "../assets/images/LOGO.png";
-
-export default function Navbar() {
-  return (
-    <nav>
-      <h2>This is the Navbar</h2>
-      <p>Notre navbar sera séparée en deux parties !</p>
-    </nav>
-  );
-}
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React, { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaBell,
+  FaBriefcase,
+  FaFileAlt,
+  FaUser,
+  FaHome,
+} from "react-icons/fa";
+import LOGO from "../assets/images/LOGO.png";
+import "../styles/navbar_top.css";
 
 // ------------- Navbar top--------------------//
 
-// export default function Navbar() {
-//   return (
-//     <nav>
-//       <div className="logo">
-//         <img src="/images/LOGO.png" alt="SheTechJobs" />
-//       </div>
-//       <ul>
-//         <li>
-//           <Link to="/">Accueil</Link>
-//         </li>
-//         <li>
-//           <Link to="/mycv">Mon CV</Link>
-//         </li>
-//         <li>
-//           <Link to="/mypostings">Mes annonces</Link>
-//         </li>
-//         <li>
-//           <Link to="/myalerts">Mes alertes</Link>
-//         </li>
-//         <li>
-//           <Link to="/myspace">Mon espace</Link>
-//         </li>
-//       </ul>
-//       <SearchBar />
-//     </nav>
-//   );
-// }
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={LOGO} alt="" />
+      </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <a href="/" />
+          <FaHome className="icon" />
+          Homepage
+        </li>
+        <li className="nav-item">
+          <a href="/" />
+          <FaFileAlt className="icon" />
+          Mon CV
+        </li>
+        <li className="nav-item">
+          <a href="/" />
+          <FaBriefcase className="icon" />
+          Mes annonces
+        </li>
+        <li className="nav-item">
+          <a href="/" />
+          <FaBell className="icon" />
+          Mes alertes
+        </li>
+        <li className="nav-item">
+          <a href="/" />
+          <FaUser className="icon" />
+          Mon espace
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
