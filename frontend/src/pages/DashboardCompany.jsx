6af00d90c6ac2879d8b9ff99e3d8d9eb9f2ef093
Maurane Populo <maurane.populo@orange.fr>
2023-11-23 17:10:39 +0100

    Création dashboard entreprise avec composant dynamique

---
+++ frontend/src/pages/DashboardCompany.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import PublishOffer from "../components/PublishOffer";
+import SpaceCompany from "../components/SpaceCompany";
+
+export default function DashbordCompany() {
+  const [displaySpaceCompany, setDisplaySpaceCompany] = useState(1);
+  const [displayPublishOffer, setDisplayPublishOffer] = useState(false);
+
+  const handleButtonClick = (componentNumber) => {
+    setDisplaySpaceCompany(componentNumber === 1);
+    setDisplayPublishOffer(componentNumber === 2);
+  };
+
+  return (
+    <div className="dashboardcompany">
+      <div className="nav_dashboard_company">
+        <ul className="ul_dashboard_company">
+          <li className="middle">
+            <button
+              type="button"
+              className="btnDashCompany"
+              onClick={() => handleButtonClick(1)}
+            >
+              Espace Entreprise
+            </button>
+          </li>
+          <li className="middle">
+            <button
+              type="button"
+              className="btnDashCompany"
+              onClick={() => handleButtonClick(2)}
+            >
+              Publier une annonce
+            </button>
+          </li>
+          <li className="middle">
+            <button type="button" className="btnDashCompany">
+              Mes annonces
+            </button>
+          </li>
+          <li className="middle">
+            <button type="button" className="btnDashCompany">
+              Candidatures
+            </button>
+          </li>
+          <li className="middle">
+            <button type="button" className="btnDashCompany">
+              Gérer son compte
+            </button>
+          </li>
+        </ul>
+      </div>
+      {/* <SpaceCompany /> */}
+      <div className="centered-dashcompanycomponents">
+        {displaySpaceCompany && <SpaceCompany />}
+        {displayPublishOffer && <PublishOffer />}
+      </div>
+    </div>
+  );
+}
